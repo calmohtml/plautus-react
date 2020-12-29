cd353ccad9a031850035d404be1d88edef28e2d7
calmohtml <contactosegovia@protonmail.com>
2020-12-29 19:50:23 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Landing page of PlautUs, a fictional financial entity.
---
+++ src/container/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "../assets/styles/App.scss";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>¡Hello, world!</h1>
+    </Fragment>
+  );
+};
+
+export default App;
